6a7dcbb1a238fcc237e6b7a179565363dd012aa0
krivopustov <krivopustov@haulmont.com>
2014-10-14 23:55:19 +0400

    UI is almost done

---
+++ script/main_view.ts
@@ -0,0 +1,90 @@
+/**
+ * Created by krivopustov on 06.10.2014.
+ */
+
+/// <reference path="../lib/jquery/jquery.d.ts" />
+/// <reference path="../lib/underscore/underscore.d.ts" />
+/// <reference path="model.ts" />
+/// <reference path="server.ts" />
+
+module mpremote {
+
+    export class MainView {
+
+        private server: Server;
+
+        constructor() {
+            this.server = new Server();
+        }
+
+        render(parent: Item): void {
+            var breadcrumbs;
+            var tableBody = $(".table tbody");
+            tableBody.empty();
+
+            breadcrumbs = $("#breadcrumbs");
+            breadcrumbs.empty();
+            breadcrumbs.append(this.createBreadcrumbs(parent));
+
+            breadcrumbs.off();
+            breadcrumbs.on("click", "a", (event) => {
+                event.preventDefault();
+                var idx = $(event.target).data("id");
+                console.log(idx);
+                if (idx >= 0) {
+                    var path = _.first(parent.getPathParts(), idx + 1).join("/");
+                    this.render(new Item({path: path, size: -1}));
+                } else {
+                    this.render(null);
+                }
+            });
+
+            var items = this.server.load(parent == null ? null : parent.getPath());
+            for (var i = 0; i < items.length; i++) {
+                tableBody.append(this.createRow(i, items[i]));
+            }
+
+            $("a.name-link").on("click", (event) => {
+                event.preventDefault();
+                this.render(items[$(event.target).data("id")]);
+            });
+
+            $(".add").on("click", (event) => {
+                this.server.add(items[$(event.target).data("id")]);
+            });
+        }
+
+        private createBreadcrumbs(parent: Item): string {
+            var res: string = "";
+            if (parent != null) {
+                var breadcrumbTemplate = $("#breadcrumb-template").html();
+                res += _.template(breadcrumbTemplate, {id: -1, name: "Root"});
+                for (var i = 0; i < parent.getPathParts().length; i++) {
+                    var part = parent.getPathParts()[i];
+                    if (i < parent.getPathParts().length - 1)
+                        res += _.template(breadcrumbTemplate, {id: i, name: part});
+                    else
+                        res += _.template($("#breadcrumb-active-template").html(), {name: part});
+                }
+            } else {
+                res += "Root";
+            }
+            return res;
+        }
+
+        private createRow(i: number, item: Item): string {
+            var itemLink: string = item.isDirectory() ?
+                _.template($("#item-link-template").html(), {itemId: i, itemName: item.getName()}) :
+                item.getName();
+
+            return _.template($("#item-template").html(),
+                {itemId: i, itemLink: itemLink, itemSize: (item.isDirectory() ? "-" : item.getSize().toString())});
+        }
+
+    }
+}
+
+$(document).ready(() => {
+    var mainView = new mpremote.MainView();
+    mainView.render(null);
+});
---
+++ script/model.ts
@@ -0,0 +1,49 @@
+/**
+ * Created by krivopustov on 08.10.2014.
+ */
+
+/// <reference path="../lib/underscore/underscore.d.ts" />
+
+module mpremote {
+
+    export class Item {
+
+        private path: string;
+        private pathParts: Array<string>;
+        private size: number;
+
+        constructor(obj: { path: string; size: number }) {
+            this.path = obj.path;
+            this.pathParts = this.path.split("/");
+            this.size = obj.size;
+        }
+
+        getPath(): string {
+            return this.path;
+        }
+
+        getSize(): number {
+            return this.size;
+        }
+
+        getName(): string {
+            return _.last(this.pathParts);
+        }
+
+        getParent() : string {
+            if (this.pathParts.length > 1) {
+                return _.initial(this.pathParts).join("/");
+            } else {
+                return null;
+            }
+        }
+
+        isDirectory(): boolean {
+            return this.size == -1;
+        }
+
+        getPathParts(): Array<string> {
+            return this.pathParts;
+        }
+    }
+}
